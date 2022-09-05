b2264f6664ca365512a8436f092b6ce98c712103
Sweater2004 <102852341+Sweater2004@users.noreply.github.com>
2022-09-05 11:06:03 +0700

    Add files via upload

---
+++ vacancy.php
@@ -0,0 +1,99 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+if (!$USER->IsAdmin()) {
+    LocalRedirect('/');
+}
+\Bitrix\Main\Loader::includeModule('iblock');
+$row = 1;
+$IBLOCK_ID = 4;
+
+$el = new CIBlockElement;
+$arProps = [];
+
+$rsElement = CIBlockElement::getList(
+    [],
+    ['IBLOCK_ID' => 4],
+    false,
+    false,
+    ['ID', 'NAME']
+);
+
+
+$rsProp = CIBlockPropertyEnum::GetList(
+    ["SORT" => "ASC", "VALUE" => "ASC"],
+    ['IBLOCK_ID' => $IBLOCK_ID]
+);
+
+while ($arProp = $rsProp->Fetch()) {
+    $key = trim($arProp['VALUE']);
+    $arProps[$arProp['PROPERTY_CODE']][$key] = $arProp['ID'];
+}
+$rsElements = CIBlockElement::GetList([], ['IBLOCK_ID' => $IBLOCK_ID], false, false, ['ID']);
+while ($element = $rsElements->GetNext()) {
+    CIBlockElement::Delete($element['ID']);
+}
+
+if (($handle = fopen("vacancy.csv", "r")) !== false) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+        if ($row == 1) {
+            $row++;
+            continue;
+        }
+        $row++;
+
+        $PROP['IMA'] = $data[1];
+        $PROP['SURNAME'] = $data[2];
+        $PROP['GENDER'] = $data[4];
+        $PROP['RESIDENCE'] = $data[5];
+
+        foreach ($PROP as $key => &$value) {
+            $value = trim($value);
+            $value = str_replace('\n', '', $value);
+            if (stripos($value, '•') !== false) {
+                $value = explode('•', $value);
+                array_splice($value, 0, 1);
+                foreach ($value as &$str) {
+                    $str = trim($str);
+                }
+            } elseif ($arProps[$key]) {
+                $arSimilar = [];
+                foreach ($arProps[$key] as $propKey => $propVal) {
+                    if ($key == 'IMA') {
+                        $value = strtolower($value);
+                        if ($value == 'Илья') {
+                            $value .= 'Илья ' . $data[2];
+                        }
+                        $arSimilar[similar_text($value, $propKey)] = $propVal;
+                    }
+                    if (stripos($propKey, $value) !== false) {
+                        $value = $propVal;
+                        break;
+                    }
+
+                    if (similar_text($propKey, $value) > 50) {
+                        $value = $propVal;
+                    }
+                }
+                if ($key == 'IMA' && !is_numeric($value)) {
+                    ksort($arSimilar);
+                    $value = array_pop($arSimilar);
+                }
+            }
+        }
+        $arLoadProductArray = [
+            "MODIFIED_BY" => $USER->GetID(),
+            "IBLOCK_SECTION_ID" => false,
+            "IBLOCK_ID" => $IBLOCK_ID,
+            "PROPERTY_VALUES" => $PROP,
+            "NAME" => $data[3],
+            "ACTIVE" => end($data) ? 'Y' : 'N',
+        ];
+
+        if ($PRODUCT_ID = $el->Add($arLoadProductArray)) {
+            echo "Добавлен элемент с ID : " . $PRODUCT_ID . "<br>";
+        } else {
+            echo "Error: " . $el->LAST_ERROR . '<br>';
+        }
+    }
+    fclose($handle);
+}
